49670cdd89d2fcaa1b9d477119658838fea767be
Thibaut Jombart <tjombart@imperial.ac.uk>
2012-03-29 16:55:15 +0100

    R interface for gen likelihood now works.

--- pkg/R/interface.R
+++ pkg/R/interface.R
@@ -26,35 +26,52 @@ testrawtoC <- function(x){
 
 
 
-testSimLike <- function(mu=0.01, p.new.patient=0.5, seq.length=100, ...){
+testSimLike <- function(mu=0.01, p.new.patient=0.5, seq.length=100, min.n=10,...){
     x <- sim.mrsa(mu=mu, p.new.patient=p.new.patient, seq.length=seq.length, ...)
-    while(nrow(x$dna)<10){
+    while(nrow(x$dna)<min.n){
         x <- sim.mrsa(mu=mu, p.new.patient=p.new.patient, seq.length=seq.length, ...)
     }
 
-    from <- 1L
-    to <- 2L
-    si <- which(x$patient==to)
-    sj <- which(x$patient==from)
-    ti <- as.double(x$date[si])
-    tj <- as.double(x$date[sj])
-    res <- double(1)
-    si <- as.integer(si-1)
-    sj <- as.integer(sj-1)
+    ## get combinations of patient->patient relationships
+    npatients <- max(x$ances.pat,na.rm=TRUE)
+    temp <- lapply(1:npatients, function(i) rbind(from=setdiff(1:npatients,i),to=rep(i,npatients-1)))
+    allCombi <- Reduce(cbind,temp) # all possible infections
+
+    res <- double(0)
+
+    for(i in 1:ncol(allCombi)){
+        from <- allCombi[1,i]
+        to <- allCombi[2,i]
+        si <- which(x$patient==to)
+        sj <- which(x$patient==from)
+        ti <- as.double(x$date[si])
+        tj <- as.double(x$date[sj])
+        si <- as.integer(si-1)
+        sj <- as.integer(sj-1)
 
 
 
-##test_genlike(unsigned char *DNAbinInput, int *n, int *length, int *s_i, , int *s_j, double *t_i, double *t_j, int *m_i, int *m_j, double *nu1, double *nu2, double *alpha, double *tau, double *out)
+        ##test_genlike(unsigned char *DNAbinInput, int *n, int *length, int *s_i, , int *s_j, double *t_i, double *t_j, int *m_i, int *m_j, double *nu1, double *nu2, double *alpha, double *tau, double *out)
 
-    temp <- .C("test_genlike", unlist(as.list(x$dna),use.names=FALSE), nrow(x$dna), ncol(x$dna),
-               si, sj, ti, tj, length(si), length(sj), as.double(mu), as.double(mu),
-               as.double(p.new.patient), as.double(1), double(1), PACKAGE="outbreaker")
-    res <- temp[14]
-    return(exp(res))
+        llik <- unlist(.C("test_genlike", unlist(as.list(x$dna),use.names=FALSE), nrow(x$dna), ncol(x$dna),
+                   si, sj, ti, tj, length(si), length(sj), as.double(mu), as.double(mu),
+                   as.double(p.new.patient), as.double(1), double(1), PACKAGE="outbreaker")[14])
+
+        res <- c(res,llik)
+    }
+
+    ## identify actual infections in allCombi ##
+    candi <- apply(allCombi,2,paste, collapse="-")
+    real <- apply(x$ances.pat,1,paste, collapse="-")
+
+
+    res <- data.frame(from=allCombi[1,],to=allCombi[2,],actual=candi %in% real,likelihood=res)
+    return(res)
 }
 
 ##
 ##  library(outbreaker)
-##
+##  test <- testSimLike()
+##  qplot(likelihood,data=test,colour=actual, geom="density")
 ##
 ##
