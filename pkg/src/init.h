3240c071b59455de8bc597ad46efb09d0abec725
Thibaut Jombart <tjombart@imperial.ac.uk>
2012-04-02 16:30:35 +0100

    Merging Anne's code into pkg. Works. rng moved from external to parameters. pkg genet example OK.

---
+++ pkg/src/init.h
@@ -0,0 +1,17 @@
+#ifndef __COMMON_H
+#include "common.h"
+#endif
+
+#ifndef __INIT_H
+#define __INIT_H
+
+void CalculIsInHosp(nb_data *, raw_data *);
+
+void InitAugData(parameters *, nb_data * , raw_data *, aug_data *);
+
+void InitMCMCSettings(mcmcInternals *);
+
+void InitParam(parameters *param);
+
+#endif
+
