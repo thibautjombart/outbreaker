9930fb9b9df16b7e7bbd8af611b97f3946a266f2
Thibaut Jombart <tjombart@imperial.ac.uk>
2012-04-02 16:31:34 +0100

    Forgot that boy

---
+++ pkg/src/InputOutput.h
@@ -0,0 +1,18 @@
+#ifndef __COMMON_H
+#include "common.h"
+#endif
+
+#ifndef __INPUTOUTPUT_H
+#define __INPUTOUTPUT_H
+
+void readFakeNbData(nb_data *);
+
+void readFakeData(nb_data *, raw_data *);
+
+void prepAllFiles(output_files *);
+
+void writeAllFiles(output_files * ,parameters * , nb_data *, raw_data * , aug_data *);
+
+#endif
+
+
