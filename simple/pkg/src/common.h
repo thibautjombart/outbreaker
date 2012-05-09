5d1acd0e2e26af56aa22231b99f83706570ae25a
Thibaut Jombart <tjombart@imperial.ac.uk>
2012-05-09 18:53:04 +0100

    Added all new files for simple model

---
+++ simple/pkg/src/common.h
@@ -0,0 +1,205 @@
+/*
+  Coded by Thibaut Jombart (t.jombart@imperial.ac.uk) and Anne Cori (a.cori@imperial.ac.uk), 2012.
+  Licence: GPL >=2.
+
+*/
+
+#ifndef __COMMON_H
+#define __COMMON_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+
+/* Calls to GNU Scientific Library */
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_cdf.h>
+#include <gsl/gsl_eigen.h>
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_math.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_permutation.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_sf.h>
+#include <gsl/gsl_sf_gamma.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_sort.h>
+
+#define NEARZERO 0.0000000001
+#define TRUE 1
+#define FALSE 0
+
+#define SizeWard0 8
+#define SizeWard1 12
+/* #define NbPatients 142 */
+/* #define T 100 /\* number of time steps corresponding to the study period *\/ */
+
+typedef short int bool;
+
+
+
+typedef struct{
+    int NbPatients, T, NbSequences; /* T: number of time steps corresponding to the study period */
+    int * NbAdmissions; /* vector of size NbPatients */
+    int * NbPosSwabs; /* vector of size NbPatients */
+    int * NbNegSwabs; /* vector of size NbPatients */
+    int NbColonisedPatients; /* those who have at least one positive swab */
+    int * indexColonisedPatients; /* those who have at least one positive swab */
+    int * M; /* number of sequences in each patient */
+} nb_data;
+
+typedef struct{
+    /* EPI DATA */
+    int NbPatients, T, NbSequences; /* T: number of time steps corresponding to the study period */
+    int * ward; /* vector of length NbPatients, 0 for adult, 1 for pediatric */
+    /* int * timeSeq; /\* time of sequence for each patient *\/ */
+    /* int * PatientIndex; /\* vector from 0 to (NbPatients-1) *\/ */
+    gsl_vector ** A; /* admission times. A[i] is a vector of size nb_data->NbAdmissions[i] */
+    gsl_vector ** D; /* discharge times. D[i] is a vector of size nb_data->NbAdmissions[i] */
+    gsl_vector ** P; /* times of positive swabs. P[i] is a vector of size nb_data->NbPosSwabs[i] */
+    gsl_vector ** N; /* times of negative swabs. N[i] is a vector of size nb_data->NbNegSwabs[i] */
+    gsl_vector ** IsInHosp; /* 0 when patient is outside hospital, 1 when inside. IsInHosp[i] is a vector of size T */
+
+    /* GENETIC DATA */
+    int ** S; /* indices of sequences collected for each patient; S[i] has length M[i] */
+    int * Tcollec; /* collection times of the sequences; length: NbSequences*/
+    int * M; /* number of sequences collected for each patient */
+
+    /* RANDOM NUMBER GENERATOR */
+    gsl_rng * rng; /* nice here since raw data don't change */
+} raw_data;
+
+
+
+
+typedef struct{
+    int NbPatients, T, NbSequences;
+    int *C; /* colonisation times */
+    int *E; /* times of end of colonisation */
+    int *I0; /* number of colonised individuals in ward 0 at each time step */
+    int *I1; /* number of colonised individuals in ward 1 at each time step */
+    vec_double **B; /* indic of direct ancestries; **B has length NbPatients; B[i]: ances. indic if sequences in patient 'i' */
+    /* gsl_matrix *alpha; /\* proba of direct ancestry between any 2 sequences; square matrix of dim NbSequences *\/ */
+    /* gsl_matrix *tau; /\* TMRCA offset between any 2 sequences; square matrix of dim NbSequences *\/ */
+} aug_data;
+
+
+
+typedef struct{
+    /* to be estimated */
+    gsl_matrix *beta; /* person to person transmission rates between and within wards */
+    double betaWardOut; /* force of transmission from outside applied to patients in the wards */
+    double betaOutOut; /* force of transmission applied to individuals outside the wards */
+
+    /* double Sp; /\* specificity of the test *\/ assumed = 100% */
+    double Se; /* sensibility of the test */
+
+    double Pi; /* probability of being colonized at first admission */
+
+    double mu; /* mean duration of colonisation */
+    double sigma; /* std of the duration of colonisation */
+    /* *************** MAYBE NEED TO REPARAMETERIZE MU, SIGMA INTO MU, CV *************** */
+
+    double nu1; /* rate of transitions */
+    /* double nu2; /\* rate of tranversions *\/ */
+    double kappa; /* nu2 = kappa*nu1 */
+
+    /* double tau; /\* time to the most recent common ancestor *\/ */
+    /* double alpha; /\* probability that two sampled isolates belong to the same lineage *\/ */
+    double weightNaGen; /* weight used to replace missing genetic likelihood */
+} parameters;
+
+
+typedef struct{
+    double mu;  //mean duration of hospitalisation
+    double sigma;  //std of the duration of hospitalisation
+} hospDurationParam;
+
+
+
+
+
+typedef struct{
+    long NbSimul; /* nb of iterations of Metropolis-Hastings */
+    int SubSample; /* results recorded every SubSample iterations */
+    int BurnIn; /* nb of iterations considered as the Burn in period */
+
+    /* standard deviation for the proposition laws */
+    gsl_matrix *Sigma_beta;
+    double Sigma_betaWardOut;
+    double Sigma_betaOutOut;
+    double Sigma_mu;
+    double Sigma_sigma;
+    double Sigma_nu1;
+    double Sigma_kappa;
+    double Sigma_tau;
+    double Sigma_alpha;
+} mcmcInternals;
+
+
+
+
+
+typedef struct{
+    /* average probabilities of acceptance */
+    gsl_matrix *PourcAcc_beta;
+    double PourcAcc_betaWardOut;
+    double PourcAcc_betaOutOut;
+    double PourcAcc_mu;
+    double PourcAcc_sigma;
+    double PourcAcc_nu1;
+    double PourcAcc_kappa;
+    double PourcAcc_tau;
+    double PourcAcc_alpha;
+
+} acceptance;
+
+
+
+
+
+typedef struct{
+    gsl_matrix *IsAccOK_beta;
+    double IsAccOK_betaWardOut;
+    double IsAccOK_betaOutOut;
+    double IsAccOK_mu;
+    double IsAccOK_sigma;
+    double IsAccOK_nu1;
+    double IsAccOK_kappa;
+    double IsAccOK_tau;
+    double IsAccOK_alpha;
+} isAcceptOK;
+
+
+
+
+
+typedef struct{
+    gsl_matrix *NbProp_beta;
+    double NbProp_betaWardOut;
+    double NbProp_betaOutOut;
+    double NbProp_mu;
+    double NbProp_sigma;
+    double NbProp_nu1;
+    double NbProp_kappa;
+    double NbProp_tau;
+    double NbProp_alpha;
+} NbProposals;
+
+
+
+
+
+typedef struct{
+    FILE *LogL;
+    FILE *Parameters;
+    FILE *ColonDates;
+    FILE *EndColonDates;
+} output_files;
+
+
+
+#endif
---
+++ simple/pkg/src/tuneVariances.c
@@ -0,0 +1,304 @@
+#include "common.h"
+#include "init.h"
+#include "InputOutput.h"
+#include "logL.h"
+#include "mcmc.h"
+#include "moves.h"
+#include "prior.h"
+#include "alloc.h"
+#include "tuneVariances.h"
+
+/* extern gsl_rng * rng; */
+
+
+int IsAcceptOKBeta(int i, int j, isAcceptOK * acceptOK){
+    int res=1;
+    if(gsl_matrix_get(acceptOK->IsAccOK_beta,i,j) < 3){res=0;}
+    return res;
+}
+
+
+
+
+
+int IsAcceptOKBetaWardOut(isAcceptOK * acceptOK){
+    int res=1;
+    if(acceptOK->IsAccOK_betaWardOut < 3){res=0;}
+    return res;
+}
+
+
+
+
+
+
+int IsAcceptOKBetaOutOut(isAcceptOK * acceptOK){
+    int res=1;
+    if(acceptOK->IsAccOK_betaOutOut < 3){res=0;}
+    return res;
+}
+
+
+
+
+
+
+int IsAcceptOKMu(isAcceptOK * acceptOK){
+    int res=1;
+    if(acceptOK->IsAccOK_mu < 3){res=0;}
+    return res;
+}
+
+
+
+
+
+
+int IsAcceptOKSigma(isAcceptOK * acceptOK){
+    int res=1;
+    if(acceptOK->IsAccOK_sigma < 3){res=0;}
+    return res;
+}
+
+
+
+
+
+
+int IsAcceptOKNu1(isAcceptOK * acceptOK){
+    int res=1;
+    if(acceptOK->IsAccOK_nu1 < 3){res=0;}
+    return res;
+}
+
+
+
+
+
+
+int IsAcceptOKKappa(isAcceptOK * acceptOK){
+    int res=1;
+    if(acceptOK->IsAccOK_kappa < 3){res=0;}
+    return res;
+}
+
+
+
+
+
+
+/* int IsAcceptOKTau(isAcceptOK * acceptOK){ */
+/*     int res=1; */
+/*     if(acceptOK->IsAccOK_tau < 3){res=0;} */
+/*     return res; */
+/* } */
+
+
+
+
+
+
+/* int IsAcceptOKAlpha(isAcceptOK * acceptOK){ */
+/*     int res=1; */
+/*     if(acceptOK->IsAccOK_alpha < 3){res=0;} */
+/*     return res; */
+/* } */
+
+
+
+
+
+
+void updateAccept(NbProposals * nbProp, acceptance * accept){
+    int i,j;
+    double prov;
+
+    for(i=0 ; i<2 ; i++)
+	{
+	    for(j=0 ; j<2 ; j++)
+		{
+		    prov=gsl_matrix_get(accept->PourcAcc_beta,i,j);
+		    gsl_matrix_set(accept->PourcAcc_beta,i,j,prov/gsl_matrix_get(nbProp->NbProp_beta,i,j));
+		}
+	}
+
+    accept->PourcAcc_betaWardOut/=nbProp->NbProp_betaWardOut;
+    accept->PourcAcc_betaOutOut/=nbProp->NbProp_betaOutOut;
+
+    accept->PourcAcc_mu/=nbProp->NbProp_mu;
+    accept->PourcAcc_sigma/=nbProp->NbProp_sigma;
+
+    accept->PourcAcc_nu1/=nbProp->NbProp_nu1;
+    accept->PourcAcc_kappa/=nbProp->NbProp_kappa;
+}
+
+
+
+
+
+
+void updateMCMCSettingsBeta(int i, int j, NbProposals * nbProp,acceptance * accept,isAcceptOK *acceptOK, mcmcInternals *MCMCSettings){
+    double prov,perc;
+
+    perc=gsl_matrix_get(accept->PourcAcc_beta,i,j)/gsl_matrix_get(nbProp->NbProp_beta,i,j);
+    if(perc<0.1 || perc>0.4)
+	{
+	    prov=gsl_matrix_get(MCMCSettings->Sigma_beta,i,j);
+	    gsl_matrix_set(MCMCSettings->Sigma_beta,i,j,prov*perc/0.25);
+	    gsl_matrix_set(acceptOK->IsAccOK_beta,i,j,0);
+	}else
+	{
+	    prov=gsl_matrix_get(acceptOK->IsAccOK_beta,i,j);
+	    gsl_matrix_set(acceptOK->IsAccOK_beta,i,j,prov+1);
+	}
+}
+
+
+
+
+
+
+void updateMCMCSettingsBetaWardOut(NbProposals * nbProp,acceptance * accept,isAcceptOK *acceptOK, mcmcInternals *MCMCSettings){
+    double perc;
+
+    perc=accept->PourcAcc_betaWardOut/nbProp->NbProp_betaWardOut;
+    if(perc<0.1 || perc>0.4)
+	{
+	    MCMCSettings->Sigma_betaWardOut*=perc/0.25;
+	    acceptOK->IsAccOK_betaWardOut=0;
+	}else
+	{
+	    acceptOK->IsAccOK_betaWardOut+=1;
+	}
+}
+
+
+
+
+
+
+void updateMCMCSettingsBetaOutOut(NbProposals * nbProp,acceptance * accept,isAcceptOK *acceptOK, mcmcInternals *MCMCSettings){
+    double perc;
+
+    perc=accept->PourcAcc_betaOutOut/nbProp->NbProp_betaOutOut;
+    if(perc<0.1 || perc>0.4)
+	{
+	    MCMCSettings->Sigma_betaOutOut*=perc/0.25;
+	    acceptOK->IsAccOK_betaOutOut=0;
+	}else
+	{
+	    acceptOK->IsAccOK_betaOutOut+=1;
+	}
+}
+
+
+
+
+
+
+void updateMCMCSettingsMu(NbProposals * nbProp,acceptance * accept,isAcceptOK *acceptOK, mcmcInternals *MCMCSettings){
+    double perc;
+
+    perc=accept->PourcAcc_mu/nbProp->NbProp_mu;
+    if(perc<0.1 || perc>0.4)
+	{
+	    MCMCSettings->Sigma_mu*=perc/0.25;
+	    acceptOK->IsAccOK_mu=0;
+	}else
+	{
+	    acceptOK->IsAccOK_mu+=1;
+	}
+}
+
+
+
+
+
+
+void updateMCMCSettingsSigma(NbProposals * nbProp,acceptance * accept,isAcceptOK *acceptOK, mcmcInternals *MCMCSettings){
+    double perc;
+
+    perc=accept->PourcAcc_sigma/nbProp->NbProp_sigma;
+    if(perc<0.1 || perc>0.4)
+	{
+	    MCMCSettings->Sigma_sigma*=perc/0.25;
+	    acceptOK->IsAccOK_sigma=0;
+	}else
+	{
+	    acceptOK->IsAccOK_sigma+=1;
+	}
+}
+
+
+
+
+
+
+void updateMCMCSettingsNu1(NbProposals * nbProp,acceptance * accept,isAcceptOK *acceptOK, mcmcInternals *MCMCSettings){
+    double perc;
+
+    perc=accept->PourcAcc_nu1/nbProp->NbProp_nu1;
+    if(perc<0.1 || perc>0.4)
+	{
+	    MCMCSettings->Sigma_nu1*=perc/0.25;
+	    acceptOK->IsAccOK_nu1=0;
+	}else
+	{
+	    acceptOK->IsAccOK_nu1+=1;
+	}
+}
+
+
+
+
+
+void updateMCMCSettingsKappa(NbProposals * nbProp,acceptance * accept,isAcceptOK *acceptOK, mcmcInternals *MCMCSettings){
+    double perc;
+
+    perc=accept->PourcAcc_kappa/nbProp->NbProp_kappa;
+    if(perc<0.1 || perc>0.4)
+	{
+	    MCMCSettings->Sigma_kappa*=perc/0.25;
+	    acceptOK->IsAccOK_kappa=0;
+	}else
+	{
+	    acceptOK->IsAccOK_kappa+=1;
+	}
+}
+
+
+
+
+
+
+/* void updateMCMCSettingsTau(NbProposals * nbProp,acceptance * accept,isAcceptOK *acceptOK, mcmcInternals *MCMCSettings){ */
+/*     double perc; */
+
+/*     perc=accept->PourcAcc_tau/nbProp->NbProp_tau; */
+/*     if(perc<0.1 || perc>0.4) */
+/* 	{ */
+/* 	    MCMCSettings->Sigma_tau*=perc/0.25; */
+/* 	    acceptOK->IsAccOK_tau=0; */
+/* 	}else */
+/* 	{ */
+/* 	    acceptOK->IsAccOK_tau+=1; */
+/* 	} */
+/* } */
+
+
+
+
+
+/* void updateMCMCSettingsAlpha(NbProposals * nbProp,acceptance * accept,isAcceptOK *acceptOK, mcmcInternals *MCMCSettings){ */
+/*     double perc; */
+
+/*     perc=accept->PourcAcc_alpha/nbProp->NbProp_alpha; */
+/*     if(perc<0.1 || perc>0.4) */
+/* 	{ */
+/* 	    MCMCSettings->Sigma_alpha*=perc/0.25; */
+/* 	    acceptOK->IsAccOK_alpha=0; */
+/* 	}else */
+/* 	{ */
+/* 	    acceptOK->IsAccOK_alpha+=1; */
+/* 	} */
+/* } */
