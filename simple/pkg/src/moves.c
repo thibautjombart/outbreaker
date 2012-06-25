472cf904e2c4aea0513fbca51d2a1c6c5ab47cba
Thibaut Jombart <tjombart@imperial.ac.uk>
2012-06-25 17:37:47 +0100

    MCMC algo implemented. Works, valgrinded. No auto-optimization yet; not tested in outbreaker.

--- simple/pkg/src/moves.c
+++ simple/pkg/src/moves.c
@@ -336,158 +336,158 @@ CODE MODEL FROM ANNE
 >>>> TESTING <<<<
 */
 
-int main(){
-  /* DECLARATIONS */
-    int TIMESPAN, i;
-    data *dat;
-    gentime *gen;
-    param *par, *tempPar;
-    dna_dist * dnainfo;
-    mcmc_param * mcmcPar;
+/* int main(){ */
+/*   /\* DECLARATIONS *\/ */
+/*     int TIMESPAN, i; */
+/*     data *dat; */
+/*     gentime *gen; */
+/*     param *par, *tempPar; */
+/*     dna_dist * dnainfo; */
+/*     mcmc_param * mcmcPar; */
 
-    double logPrior, logLike, logPost;
+/*     double logPrior, logLike, logPost; */
 
-    /* INITIALIZE RNG */
-    gsl_rng *rng = create_gsl_rng(time(NULL));
-
-
-    /* CONVERT DATA */
-    dat = alloc_data(3,10);
-    dat->dates->values[0] = 0;
-    dat->dates->values[1] = 2;
-    dat->dates->values[1] = 3;
-    dat->dna->list[0]->seq[0] = 'a';
-    dat->dna->list[1]->seq[0] = 'a';
-    dat->dna->list[2]->seq[0] = 't';
-    printf("\n>>> Data <<<\n");
-    print_data(dat);
+/*     /\* INITIALIZE RNG *\/ */
+/*     gsl_rng *rng = create_gsl_rng(time(NULL)); */
 
 
-    /* GET TIME SPAN */
-    TIMESPAN = max_vec_int(dat->dates) - min_vec_int(dat->dates);
-    printf("\nTimespan is %d\n",TIMESPAN);
+/*     /\* CONVERT DATA *\/ */
+/*     dat = alloc_data(3,10); */
+/*     dat->dates->values[0] = 0; */
+/*     dat->dates->values[1] = 2; */
+/*     dat->dates->values[1] = 3; */
+/*     dat->dna->list[0]->seq[0] = 'a'; */
+/*     dat->dna->list[1]->seq[0] = 'a'; */
+/*     dat->dna->list[2]->seq[0] = 't'; */
+/*     printf("\n>>> Data <<<\n"); */
+/*     print_data(dat); */
 
 
-    /* CREATE AND INIT GENERATION TIME */
-    gen = alloc_gentime(TIMESPAN, 5);
-    init_gentime(gen, 1, 1.0, 0.0, 0.0);
-    printf("\n>>> gentime info <<<\n");
-    print_gentime(gen);
-    printf("sizes of rows in gen: ");
-    for(i=0;i<gen->dens->n;i++) printf("%d ", gen->dens->rows[i]->length);
+/*     /\* GET TIME SPAN *\/ */
+/*     TIMESPAN = max_vec_int(dat->dates) - min_vec_int(dat->dates); */
+/*     printf("\nTimespan is %d\n",TIMESPAN); */
 
 
-     /* CREATE AND INIT PARAMETERS */
-    par = alloc_param(3);
-    par->alpha->values[0] = -1;
-    par->alpha->values[1] = 0;
-    par->alpha->values[2] = 0;
-    par->kappa->values[0] = 1;
-    par->kappa->values[1] = 1;
-    par->kappa->values[2] = 1;
-    par->mu1 = 0.0001;
-    par->gamma = 1.0;
-    par->pi = 0.5;
-    printf("\nParameters (par)\n");
-    print_param(par);
-
-    tempPar = alloc_param(3);
-    copy_param(par,tempPar);
-    printf("\nParameters (tempPar)\n");
-    print_param(par);
+/*     /\* CREATE AND INIT GENERATION TIME *\/ */
+/*     gen = alloc_gentime(TIMESPAN, 5); */
+/*     init_gentime(gen, 1, 1.0, 0.0, 0.0); */
+/*     printf("\n>>> gentime info <<<\n"); */
+/*     print_gentime(gen); */
+/*     printf("sizes of rows in gen: "); */
+/*     for(i=0;i<gen->dens->n;i++) printf("%d ", gen->dens->rows[i]->length); */
 
 
-    /* ALLOCATE MCMCPAR */
-    mcmcPar = alloc_mcmc_param(3);
-    init_mcmc_param(mcmcPar, dat);
-    printf("\nMCMC parameters (mcmcPar)\n");
-    print_mcmc_param(mcmcPar);
+/*      /\* CREATE AND INIT PARAMETERS *\/ */
+/*     par = alloc_param(3); */
+/*     par->alpha->values[0] = -1; */
+/*     par->alpha->values[1] = 0; */
+/*     par->alpha->values[2] = 0; */
+/*     par->kappa->values[0] = 1; */
+/*     par->kappa->values[1] = 1; */
+/*     par->kappa->values[2] = 1; */
+/*     par->mu1 = 0.0001; */
+/*     par->gamma = 1.0; */
+/*     par->pi = 0.5; */
+/*     printf("\nParameters (par)\n"); */
+/*     print_param(par); */
+
+/*     tempPar = alloc_param(3); */
+/*     copy_param(par,tempPar); */
+/*     printf("\nParameters (tempPar)\n"); */
+/*     print_param(par); */
 
 
-    /* COMPUTE GENETIC DISTANCES */
-    dnainfo = compute_dna_distances(dat->dna);
-    printf("\n>>> DNA info <<<\n");
-    print_dna_dist(dnainfo);
+/*     /\* ALLOCATE MCMCPAR *\/ */
+/*     mcmcPar = alloc_mcmc_param(3); */
+/*     init_mcmc_param(mcmcPar, dat); */
+/*     printf("\nMCMC parameters (mcmcPar)\n"); */
+/*     print_mcmc_param(mcmcPar); */
 
 
-    /* COMPUTE PRIORS */
-    logPrior = logprior_all(par);
-    printf("\nPrior value (log): %.10f\n", logPrior);
-
-   /* COMPUTE LIKELIHOOD */
-    logLike = loglikelihood_all(dat, dnainfo, gen, par);
-    printf("\nLog-likelihood value: %.10f\n", logLike);
-
-    /* COMPUTE POSTERIOR */
-    logPost = logposterior_all(dat, dnainfo, gen, par);
-    printf("\nLog-posterior value: %.10f\n", logPost);
+/*     /\* COMPUTE GENETIC DISTANCES *\/ */
+/*     dnainfo = compute_dna_distances(dat->dna); */
+/*     printf("\n>>> DNA info <<<\n"); */
+/*     print_dna_dist(dnainfo); */
 
 
-    /* MOVE MU1 */
-    mcmcPar->sigma_mu1 = 0.001;
-    for(i=0;i<500;i++){
-	move_mu1(par, tempPar, dat, dnainfo, mcmcPar, rng);
-	printf("\nmu1: %.10f (reject: %d  accept: %d  ratio: %.3f)", par->mu1, mcmcPar->n_reject_mu1, mcmcPar->n_accept_mu1, (double) mcmcPar->n_reject_mu1 / mcmcPar->n_accept_mu1);
-    }
-    printf("\n");
-    fflush(stdout);
+/*     /\* COMPUTE PRIORS *\/ */
+/*     logPrior = logprior_all(par); */
+/*     printf("\nPrior value (log): %.10f\n", logPrior); */
 
-    /* MOVE GAMMA */
-    for(i=0;i<500;i++){
-	move_gamma(par, tempPar, dat, dnainfo, mcmcPar, rng);
-	printf("\ngamma: %.10f (reject: %d  accept: %d  ratio: %.3f)", par->gamma, mcmcPar->n_reject_gamma, mcmcPar->n_accept_gamma, (double) mcmcPar->n_reject_gamma / mcmcPar->n_accept_gamma);
-    }
-    printf("\n");
-    fflush(stdout);
+/*    /\* COMPUTE LIKELIHOOD *\/ */
+/*     logLike = loglikelihood_all(dat, dnainfo, gen, par); */
+/*     printf("\nLog-likelihood value: %.10f\n", logLike); */
 
-    /* MOVE TINF */
-    for(i=0;i<500;i++){
-	move_Tinf(par, tempPar, dat, dnainfo, gen, mcmcPar, rng);
-	printf("\nTinf:");
-	print_vec_int(par->Tinf);
-	printf(" (reject: %d  accept: %d  ratio: %.3f)", mcmcPar->n_reject_Tinf, mcmcPar->n_accept_Tinf, (double) mcmcPar->n_reject_Tinf / mcmcPar->n_accept_Tinf);
-    }
-    printf("\n");
-    fflush(stdout);
+/*     /\* COMPUTE POSTERIOR *\/ */
+/*     logPost = logposterior_all(dat, dnainfo, gen, par); */
+/*     printf("\nLog-posterior value: %.10f\n", logPost); */
 
 
+/*     /\* MOVE MU1 *\/ */
+/*     mcmcPar->sigma_mu1 = 0.001; */
+/*     for(i=0;i<500;i++){ */
+/* 	move_mu1(par, tempPar, dat, dnainfo, mcmcPar, rng); */
+/* 	printf("\nmu1: %.10f (reject: %d  accept: %d  ratio: %.3f)", par->mu1, mcmcPar->n_reject_mu1, mcmcPar->n_accept_mu1, (double) mcmcPar->n_reject_mu1 / mcmcPar->n_accept_mu1); */
+/*     } */
+/*     printf("\n"); */
+/*     fflush(stdout); */
 
-    /* MOVE ALPHA AND KAPPA */
-    for(i=0;i<500;i++){
-	move_alpha_kappa(par, tempPar, dat, dnainfo, gen, mcmcPar, rng);
-	printf("\nAlpha:");
-	print_vec_int(par->alpha);
-	printf("\nKappa:");
-	print_vec_int(par->kappa);
-	printf(" (reject: %d  accept: %d  ratio: %.3f)", mcmcPar->n_reject_alpha, mcmcPar->n_accept_alpha, (double) mcmcPar->n_reject_alpha / mcmcPar->n_accept_alpha);
-    }
-    printf("\n");
-    fflush(stdout);
+/*     /\* MOVE GAMMA *\/ */
+/*     for(i=0;i<500;i++){ */
+/* 	move_gamma(par, tempPar, dat, dnainfo, mcmcPar, rng); */
+/* 	printf("\ngamma: %.10f (reject: %d  accept: %d  ratio: %.3f)", par->gamma, mcmcPar->n_reject_gamma, mcmcPar->n_accept_gamma, (double) mcmcPar->n_reject_gamma / mcmcPar->n_accept_gamma); */
+/*     } */
+/*     printf("\n"); */
+/*     fflush(stdout); */
+
+/*     /\* MOVE TINF *\/ */
+/*     for(i=0;i<500;i++){ */
+/* 	move_Tinf(par, tempPar, dat, dnainfo, gen, mcmcPar, rng); */
+/* 	printf("\nTinf:"); */
+/* 	print_vec_int(par->Tinf); */
+/* 	printf(" (reject: %d  accept: %d  ratio: %.3f)", mcmcPar->n_reject_Tinf, mcmcPar->n_accept_Tinf, (double) mcmcPar->n_reject_Tinf / mcmcPar->n_accept_Tinf); */
+/*     } */
+/*     printf("\n"); */
+/*     fflush(stdout); */
 
 
-    /* /\* RUNTIME TEST *\/ */
-    /* int ITER=10e6, i; */
-    /* time_t t1, t2; */
-    /* time(&t1); */
-    /* printf("\nRuntime (%d computations of posterior): \n", ITER); */
-    /* for(i=0;i<ITER;i++){ */
-    /* 	logPost = logposterior_all(dat, dnainfo, gen, par); */
-    /* } */
-    /* time(&t2); */
-    /* printf("\nellapsed time: %d seconds\n", (int) t2 - (int) t1); */
+
+/*     /\* MOVE ALPHA AND KAPPA *\/ */
+/*     for(i=0;i<500;i++){ */
+/* 	move_alpha_kappa(par, tempPar, dat, dnainfo, gen, mcmcPar, rng); */
+/* 	printf("\nAlpha:"); */
+/* 	print_vec_int(par->alpha); */
+/* 	printf("\nKappa:"); */
+/* 	print_vec_int(par->kappa); */
+/* 	printf(" (reject: %d  accept: %d  ratio: %.3f)", mcmcPar->n_reject_alpha, mcmcPar->n_accept_alpha, (double) mcmcPar->n_reject_alpha / mcmcPar->n_accept_alpha); */
+/*     } */
+/*     printf("\n"); */
+/*     fflush(stdout); */
 
 
-    /* FREE / RETURN */
-    gsl_rng_free(rng);
-    free_data(dat);
-    free_gentime(gen);
-    free_dna_dist(dnainfo);
-    free_param(par);
-    free_param(tempPar);
-    free_mcmc_param(mcmcPar);
+/*     /\* /\\* RUNTIME TEST *\\/ *\/ */
+/*     /\* int ITER=10e6, i; *\/ */
+/*     /\* time_t t1, t2; *\/ */
+/*     /\* time(&t1); *\/ */
+/*     /\* printf("\nRuntime (%d computations of posterior): \n", ITER); *\/ */
+/*     /\* for(i=0;i<ITER;i++){ *\/ */
+/*     /\* 	logPost = logposterior_all(dat, dnainfo, gen, par); *\/ */
+/*     /\* } *\/ */
+/*     /\* time(&t2); *\/ */
+/*     /\* printf("\nellapsed time: %d seconds\n", (int) t2 - (int) t1); *\/ */
 
-    return 0;
-}
+
+/*     /\* FREE / RETURN *\/ */
+/*     gsl_rng_free(rng); */
+/*     free_data(dat); */
+/*     free_gentime(gen); */
+/*     free_dna_dist(dnainfo); */
+/*     free_param(par); */
+/*     free_param(tempPar); */
+/*     free_mcmc_param(mcmcPar); */
+
+/*     return 0; */
+/* } */
 
 
 
