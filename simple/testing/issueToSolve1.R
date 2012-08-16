5a5435d6e09129a12eac5c4638616d51e8e31637
Thibaut Jombart <tjombart@imperial.ac.uk>
2012-08-16 18:01:13 +0100

    Up-to-date version.

--- simple/testing/issueToSolve1.R
+++ simple/testing/issueToSolve1.R
@@ -16,8 +16,8 @@ res <- outbreaker(dna=dat$dna, dates=dat$dates+2, w.dens=w.dens+.001, init.tree=
 res <- outbreaker(dna=dat$dna, dates=collecDates, w.dens=w.dens+.001, init.tree=dat$ances, n.iter=5)
 
 
-## init with seqTrack and random collection dates
-res <- outbreaker(dna=dat$dna, dates=collecDates, w.dens=w.dens+.001, init.tree="seqTrack", n.iter=5)
+## init with seqTrack and random collection dates, actually try to reconstruct outbreak
+res <- outbreaker(dna=dat$dna, dates=collecDates, w.dens=w.dens+.001, init.tree="seqTrack", n.iter=2e6)
 
 
 res$chain$post
@@ -25,3 +25,11 @@ res$chain$post
 plot(res$chains$step, res$chain$post, type="l", main="posterior")
 
 plot(res$chains$step[res$chains$step>2e5], res$chain$post[res$chains$step>2e5], type="l", main="posterior")
+
+
+
+## check results ##
+plot(density(res$chains$mu1[res$chains$step>2e5]), main="mu1")
+plot(density(res$chains$mu2[res$chains$step>2e5]), main="mu2")
+x <- get.TTree.simple(res)
+plot(x)
