b9e077c79ccc7b8cc9af2883568d06719ab00c09
Thibaut Jombart <tjombart@imperial.ac.uk>
2012-10-23 18:36:21 +0100

    Working on the simulation script - mostly done

--- simple/simulations/makeSimul.1.0.R
+++ simple/simulations/makeSimul.1.0.R
@@ -3,13 +3,15 @@
 ## FUNCTION MAKING SIMULATIONS:
 ## simulates data, runs outbreaker, derive results, write files
 ##
-makeSimul <- function(n=1, type=gsub(".*/","",getwd()){
+makeSimul <- function(N=1, type=gsub(".*/","",getwd()){
     ## CHECKS ##
     if(!require(outbreaker)) stop("outbreaker is needed")
     if(!require(adegenet)) stop("adegenet is needed")
     if(!require(ape)) stop("ape is needed")
     if(!require(digest)) stop("digest is needed")
     if(!require(EpiEstim)) stop("EpiEstim is needed")
+
+
 
     ## AUXILIARY FUNCTIONS ##
       w.dens.gen <- function(mu, sigma, max.k){
@@ -76,8 +78,10 @@ makeSimul <- function(n=1, type=gsub(".*/","",getwd()){
         p.seq <- 1
     }
 
+
+
     ## MAKE N SIMULATIONS ##
-    for(i in 1:n){
+    for(i in 1:N){
         ## simulate outbreak ##
         full <- simOutbreak(R0=R0, infec.curve=w, mu.transi=mu1, mu.transv=mu2, rate.import.case=r.imp,
                             duration=100, n.hosts=200, seq.length=1e4, diverg.import=10)
@@ -90,15 +94,70 @@ makeSimul <- function(n=1, type=gsub(".*/","",getwd()){
         n.dat <- round(p.samp*full$n)
         dat <- full[sort(sample(1:n.dat))]
 
+        ## check which cases are sequenced ##
+        temp <- as.logical(rbinom(dat$n, prob=p.seq, size=1))
+        which.seq <- which(temp)
+        dna <- dat$dna[which.seq,,drop=FALSE]
+
         ## get collaction dates ##
         collecDates <- dat$dates+sample(0:(length(w)-1), length(dat$dates), replace=TRUE, prob=w)
 
         ## run outbreaker ##
-        
+        BURNIN <- 2e4
+        timing <- system.time(res <- outbreaker(dna=dna, dates=collecDates, idx.dna=which.seq, w.dens=w, init.tree="star", init.kappa=NULL,
+                                      n.iter=1e5, sample.every=500,tune.every=500,burnin=BURNIN,find.import=TRUE, find.import.n=50,
+                                      pi.param1=1, pi.param2=1, init.mu1=1e-5, init.gamma=1, move.mut=TRUE, move.ances=TRUE, move.kappa=TRUE))
 
-    }
+        ## extract information from results ##
+        tre <- get.TTree.simple(res, burn=BURNIN)
+        chains <- res$chains[res$chains$step>BURNIN,,drop=FALSE]
 
-    
+        stat <- list()
+        stat$n <- dat$n
+
+        ## prop of true ancestry in consensus
+        stat$prop.ances.ok <- mean(tre$ances==dat$ances, na.rm=TRUE)
+
+        ## mean support of actual ancestor
+        alpha <- chains[,grep("alpha", names(chains))]
+        alpha[is.na(alpha)] <- -1
+        temp <- sapply(1:dat$n, function(i) mean(dat$ances[i]==alpha[,i],na.rm=TRUE))
+        stat$msup.ances <- mean(temp, na.rm=TRUE)
+
+        ## mean support of actual kappa
+        kappa <- chains[,grep("kappa", names(chains))]
+        temp <- sapply(1:dat$n, function(i) mean(dat$ngen[i]==kappa[,i],na.rm=TRUE))
+        stat$msup.kappa <-mean(temp, na.rm=TRUE)
+
+        ## mean error date
+        Tinf <- chains[,grep("Tinf", names(chains))]
+        temp <- abs(t(Tinf) - dat$dates)
+        stat$merr.date <- mean(temp, na.rm=TRUE)
+
+        ## proportion of successfully detected imported cases
+        stat$prop.imp.ok <- sum(is.na(tre$ances[-1]) & is.na(dat$ances[-1]))/sum(is.na(dat$ances[-1]))
+
+        ## mu1.ok: 1 if true value of mu1 is in the 95% CI, 0 otherwise
+        stat$mu1.ok <- as.numeric(mu1>quantile(chains$mu1, 0.05) & mu1<quantile(chains$mu1, 0.95))
+
+        ## mu2.ok: 1 if true value of mu2 is in the 95% CI, 0 otherwise
+        stat$mu2.ok <- as.numeric(mu2>quantile(chains$mu2, 0.05) & mu2<quantile(chains$mu2, 0.95))
+
+        ## merr.pi: the mean error in posterior values of pi
+        stat$merr.pi <- mean(abs(chains$pi - p.samp))
+
+        ## timing
+        stat$time <- round(timing[3])
+
+        ## hash key
+        key <- paste(unlist(strsplit(digest(dat),""))[1:10], collapse="")
+        stat <- data.frame(stat)
+        row.names(stat) <- key
+
+        ## SAVE OBJECTS TO FILE ##
+        save(full,dat,collecDates,res,chains,tre,stat,key, file=paste(key,"Rdata",sep="."))
+
+
 
 } # end makeSimul
 
