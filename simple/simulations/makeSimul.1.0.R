5b249087beacf2186b4327449a6638b4dd5d4662
Thibaut Jombart <tjombart@imperial.ac.uk>
2012-10-23 17:15:11 +0100

    Working on the simulation script

--- simple/simulations/makeSimul.1.0.R
+++ simple/simulations/makeSimul.1.0.R
@@ -1,6 +1,107 @@
-## TRY TO SEE HOW THE METHOD BEHAVES ##
 
-############################################
+################################
+## FUNCTION MAKING SIMULATIONS:
+## simulates data, runs outbreaker, derive results, write files
+##
+makeSimul <- function(n=1, type=gsub(".*/","",getwd()){
+    ## CHECKS ##
+    if(!require(outbreaker)) stop("outbreaker is needed")
+    if(!require(adegenet)) stop("adegenet is needed")
+    if(!require(ape)) stop("ape is needed")
+    if(!require(digest)) stop("digest is needed")
+    if(!require(EpiEstim)) stop("EpiEstim is needed")
+
+    ## AUXILIARY FUNCTIONS ##
+      w.dens.gen <- function(mu, sigma, max.k){
+        res <- sapply(0:max.k, DiscrSI, mu, sigma)
+        names(res) <- 0:max.k
+        return(res)
+    }
+
+
+
+    ## DEFINE PARAMETERS ##
+    ## get type of change
+    type.head <- unlist(strsplit(type,"-"))[1]
+
+    ## R0
+    if(type.head=="R"){
+        R0 <- as.numeric(unlist(strsplit(type,"-"))[2])
+    } else {
+        R0 <- 1.5
+    }
+
+    ## generation time function (w)
+    if(type.head=="w"){
+        temp <- unlist(strsplit(type,"-"))[2]
+        temp <- as.numeric(unlist(strsplit(temp, "_")))
+        w.mu <- temp[1]
+        w.sigma <- temp[2]
+        w.k <- temp[3]
+    } else {
+        w.mu <- 2
+        w.sigma <- 0.7
+        w.k <- 10
+    }
+
+    w <- w.dens.gen(w.mu, w.sigma, w.k)
+
+    ## mutation rates
+    if(type.head=="mu1"){
+       mu1 <- as.numeric(unlist(strsplit(type,"-"))[2])
+       mu2 <- 0.25*mu1
+    } else {
+        mu1 <- 0.8e-4
+        mu2 <- 0.25*mu1
+    }
+
+    ## imported cases
+     if(type.head=="imp"){
+       r.imp <- as.numeric(unlist(strsplit(type,"-"))[2])
+    } else {
+        r.imp <- 0.05
+    }
+
+    ## proportion of observed cases
+    if(type.head=="samp"){
+        p.samp <- as.numeric(unlist(strsplit(type,"-"))[2])
+    } else {
+        p.samp <- 1
+    }
+
+    ## proportion of observed cases
+    if(type.head=="seq"){
+        p.seq <- as.numeric(unlist(strsplit(type,"-"))[2])
+    } else {
+        p.seq <- 1
+    }
+
+    ## MAKE N SIMULATIONS ##
+    for(i in 1:n){
+        ## simulate outbreak ##
+        full <- simOutbreak(R0=R0, infec.curve=w, mu.transi=mu1, mu.transv=mu2, rate.import.case=r.imp,
+                            duration=100, n.hosts=200, seq.length=1e4, diverg.import=10)
+        while(full$n < 10){
+            full <- simOutbreak(R0=R0, infec.curve=w, mu.transi=mu1, mu.transv=mu2, rate.import.case=r.imp,
+                            duration=100, n.hosts=200, seq.length=1e4, diverg.import=10)
+        }
+
+        ## subset data ##
+        n.dat <- round(p.samp*full$n)
+        dat <- full[sort(sample(1:n.dat))]
+
+        ## get collaction dates ##
+        collecDates <- dat$dates+sample(0:(length(w)-1), length(dat$dates), replace=TRUE, prob=w)
+
+        ## run outbreaker ##
+        
+
+    }
+
+    
+
+} # end makeSimul
+
 ## DATA SIMULATION ##
 
 ## load packages and data
@@ -8,7 +109,6 @@ library(outbreaker)
 library(adegenet)
 library(ape)
 
-## w <- c(0,.1,.2,.5,2,.5,.2,.1)
 ## full <- simOutbreak(R0=2, infec.curve=w, mu.transi=1e-4, mu.transv=0.2e-4)
 ## dat <- full[1:20]
 ## collecDates <- dat$dates+sample(0:(length(w)-1), length(dat$dates), replace=TRUE, prob=w)
