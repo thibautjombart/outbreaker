e1a6a7c324bafd48d994cecd1ad5c9f7f20dc62f
Thibaut Jombart <tjombart@imperial.ac.uk>
2012-11-23 18:10:57 +0000

    adding scripts for re-analysis

---
+++ simple/simulations/startReanalysis.R
@@ -0,0 +1,4 @@
+source("~/dev/outbreaker/outbreaker-code/simple/simulations/poolResults.R")
+source("~/dev/outbreaker/outbreaker-code/simple/simulations/reanalyseSimul.1.0.R")
+
+sapply(dir(),reanalyseSimul)
