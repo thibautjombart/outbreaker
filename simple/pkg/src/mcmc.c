48df8e7c5e68d6fe3a70e8dbf735d6c3e5110af1
Thibaut Jombart <tjombart@imperial.ac.uk>
2012-11-23 14:36:06 +0000

    changed threshold for outliers to 1/1000

--- simple/pkg/src/mcmc.c
+++ simple/pkg/src/mcmc.c
@@ -361,9 +361,9 @@ void mcmc_find_import(vec_int *areOutliers, int outEvery, int tuneEvery, bool qu
     /* browse each likelihood, define outliers */
     /* printf("\n\nLooking for outliers...\n"); */
     for(j=0;j<dat->n;j++){
-	/* outliers = likelihood 100 times lower than the mean */
+	/* outliers = likelihood 1000 times lower than the mean */
 	/* printf("\nIndiv %d: loglike difference= %.5f", j+1, medLogLike - vec_double_i(indivLogLike,j));fflush(stdout); */
-	if((medLogLike - vec_double_i(indivLogLike,j)) > log(100)){
+	if((medLogLike - vec_double_i(indivLogLike,j)) > log(1000)){
 	    areOutliers->values[j] = 1;
 	    printf("\nIndividual %d identified as imported case\n",j+1);fflush(stdout);
 	} else {
