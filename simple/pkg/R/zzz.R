b66c427966e3abe718b5477c6527a930e5e51773
Thibaut Jombart <tjombart@imperial.ac.uk>
2013-06-05 18:40:41 +0100

    Fixed length of lines in doc

--- simple/pkg/R/zzz.R
+++ simple/pkg/R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname){
     pkg.version <- packageDescription("outbreaker", fields = "Version")
 
-    startup.txt <- paste("\n   ===    outbreaker", pkg.version, "is loaded ===   \n\n")
+    startup.txt <- paste("\n   === outbreaker", pkg.version, "is loaded ===   \n\n")
     packageStartupMessage(startup.txt)
 }
