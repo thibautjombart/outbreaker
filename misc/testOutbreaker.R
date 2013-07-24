3cb9f28c63bb3dabe4ef9dc6c648385baa95a6d8
Thibaut Jombart <tjombart@imperial.ac.uk>
2013-07-24 16:32:54 +0100

    latest version - fixed a couple of minor things, updated expl in misc

--- misc/testOutbreaker.R
+++ misc/testOutbreaker.R
@@ -1,8 +1,11 @@
 
 library(outbreaker)
 w <- c(0, 0.5, 1, 0.75)
+set.seed(2)
 
 ## NON-SPATIAL SIMULATION ##
+
+## this may generate an error if outbreak doesn't take off
 dat <- simOutbreak(R0 = 2, infec.curve = w, n.hosts = 100, spatial=FALSE)[1:15]
 collecDates <- dat$onset + sample(0:3, size=length(dat$onset), replace=TRUE, prob=w)
 
@@ -15,6 +18,7 @@ res <-  outbreaker(dna=dat$dna, dates=collecDates,w.dens=w, n.iter=5e4, init.spa
 
 dat$ances
 get.tTree(res)$ances
+mean(dat$ances == get.tTree(res)$ances, na.rm=TRUE)
 
 
 
@@ -23,6 +27,7 @@ get.tTree(res)$ances
 library(outbreaker)
 w <- c(0, 0.5, 1, 0.75)
 
+## this may generate an error if outbreak doesn't take off
 dat <- simOutbreak(R0 = .5, infec.curve = w, n.hosts = 100, spatial=TRUE)[1:50]
 collecDates <- dat$onset + sample(0:3, size=length(dat$onset), replace=TRUE, prob=w)
 D <- as.matrix(dist(dat$xy))
@@ -31,24 +36,32 @@ res <-  outbreaker(dna=dat$dna, dates=collecDates,w.dens=w, dist.mat=D, n.iter=5
 
 dat$ances
 get.tTree(res)$ances
+mean(dat$ances == get.tTree(res)$ances, na.rm=TRUE)
 
+dist.inf <- na.omit(sapply(1:dat$n, function(i) D[dat$id[i], dat$ances[i]]))
+mean(dist.inf)
+
+par(mfrow=c(2,2))
 plotChains(res)
 plotChains(res, what="spa1")
 plotChains(res, what="spa1", type="dens")
-
-hist(sapply(1:dat$n, function(i) D[dat$id[i], dat$ances[i]]), col="grey",
+abline(v=mean(dist.inf))
+hist(dist.inf , col="grey",
      main="Distance of infectious contacts", xlab="Distance infector-infected")
+
+
 
 
 ## bigger example ##
 library(outbreaker)
 w <- c(0, 0.5, 1, 0.75)
 
+## this may generate an error if outbreak doesn't take off
 dat <- simOutbreak(R0 = .5, infec.curve = w, n.hosts = 200, spatial=TRUE)[1:50]
 collecDates <- dat$onset + sample(0:3, size=length(dat$onset), replace=TRUE, prob=w)
 D <- as.matrix(dist(dat$xy))
 
-res <-  outbreaker.parallel(3, dna=dat$dna, dates=collecDates,w.dens=w, dist.mat=D, n.iter=1e5, spa.model=1)
+res <-  outbreaker.parallel(5, dna=dat$dna, dates=collecDates,w.dens=w, dist.mat=D, n.iter=1e5, spa.model=1)
 
 
 dat$ances
