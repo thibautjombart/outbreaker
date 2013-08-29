e3cd66a17bf68edc8c63c5ace52b3a911240ef97
Thibaut Jombart <tjombart@imperial.ac.uk>
2013-08-29 09:37:32 +0100

    tried to reproduce the very rare -Inf influence problem; no success out of 34 attempts; postponing for now

--- misc/importPb/testProblem.R
+++ misc/importPb/testProblem.R
@@ -1,6 +1,10 @@
 
 library(outbreaker)
-#load("bugSmall.RData")
+
+##load("bugSmall.RData")
 
 load("bug.RData")
-res <- outbreaker(dna=dat$dna, dates=collecDates, w.dens=w, idx.dna=which.seq, n.iter=5e4)
+
+which.seq <- c(1,sample(2:114, 10))
+which.seq <- c(1,1)
+res <- outbreaker(dna=dat$dna[which.seq], dates=collecDates, w.dens=w, idx.dna=which.seq, n.iter=5e4)
