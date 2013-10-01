090c52878a4ebca22a4544a7f32c3ca606b42962
Thibaut Jombart <tjombart@imperial.ac.uk>
2013-10-01 11:38:06 +0100

    removing debugging symbols. Movements of Tinf can naturally generate negative infection times, but unclear that cleverer proposal would actually speed things up. Convergence pb looks sorted. Trying to move different proportions of the ancestries now.

--- misc/testOutbreaker.R
+++ misc/testOutbreaker.R
@@ -34,7 +34,7 @@ plot(dat)
 ##     dev.off()
 ## }
 
-res <-  outbreaker.parallel(n.runs=6, dna=dat$dna, dates=collecDates,w.dens=w, n.iter=2e5, find.import=TRUE)
+res <-  outbreaker.parallel(n.runs=6, dna=dat$dna, dates=collecDates,w.dens=w, n.iter=1e5, find.import=TRUE)
 plotChains(res)
 
 
