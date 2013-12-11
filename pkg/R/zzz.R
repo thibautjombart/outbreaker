758d8c8221bef938bb1b625c1653034cfb4cff63
Thibaut Jombart <tjombart@imperial.ac.uk>
2013-12-11 10:42:00 +0700

    added a link to R-epi project

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -2,5 +2,6 @@
     pkg.version <- packageDescription("outbreaker", fields = "Version")
 
     startup.txt <- paste("\n   === outbreaker", pkg.version, "is loaded ===   \n\n")
+    startup.txt <- paste(startup.txt, "Questions/documentation -> check the R-epi project: \nhttp://sites.google.com/site/therepiproject", sep="\n")
     packageStartupMessage(startup.txt)
 }
