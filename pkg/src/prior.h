006bbc24fb1d6536ee6462048269e11094eead05
Thibaut Jombart <tjombart@imperial.ac.uk>
2014-06-10 01:22:18 +0100

    updated header for prior.c

--- pkg/src/prior.h
+++ pkg/src/prior.h
@@ -12,11 +12,11 @@ double gsl_ran_exponential_pdf_fixed(double x, double mu);
 
 double logprior_pi(param *par);
 
+double logprior_phi(param *par);
+
 double logprior_spa1(param *par);
 
 double logprior_spa2(param *par);
-
-/* double logprior_phi(param *par); */
 
 double logprior_mu1(param *par);
 
