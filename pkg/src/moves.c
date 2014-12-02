b17acb2c534c20901f84dcf01db6cdb3e38386e8
Thibaut Jombart <thibautjombart@gmail.com>
2014-12-02 17:35:57 +0000

    fixed movement of Tinf; truly symmetric proposal in move_Tinf_alpha_kappa; now using +/- 1  * ~Poisson(1) for movements of Tinf - no convergence otherwise.

--- pkg/src/moves.c
+++ pkg/src/moves.c
@@ -433,7 +433,7 @@ void move_Tinf(param *currentPar, param *tempPar, data *dat, dna_dist *dnaInfo, 
 	toMove = vec_int_i(mcmcPar->idx_move_Tinf,i);
 
 	/* move i-th Tinf */
-	tempPar->Tinf->values[toMove] += (gsl_rng_uniform(rng) >= 0.5 ? 1 : -1);
+	tempPar->Tinf->values[toMove] += (gsl_rng_uniform(rng) >= 0.5 ? 1 : -1) * gsl_ran_poisson(rng, 1);
 
 	/* /\* MAY NEED TO CHANGE THIS AND ADD CORRECTION *\/ */
 	/* /\* constraint: Tinf_i < t_i *\/ */
@@ -514,7 +514,7 @@ void move_Tinf_alpha_kappa(param *currentPar, param *tempPar, data *dat, dna_dis
 	firstImported = find_date_first_import(dat, currentPar);
 
 	/* move i-th Tinf */
-	tempPar->Tinf->values[toMove] += (gsl_rng_uniform(rng) >= 0.5 ? 1 : -1);
+	tempPar->Tinf->values[toMove] += (gsl_rng_uniform(rng) >= 0.5 ? 1 : -1) * gsl_ran_poisson(rng, 1);
 
 	/* /\* constraint: Tinf_i < t_i *\/ */
 	/* if(vec_int_i(tempPar->Tinf,toMove) >= vec_int_i(dat->dates,toMove)) tempPar->Tinf->values[toMove] = vec_int_i(dat->dates,toMove)-1; */
@@ -680,7 +680,6 @@ void move_Tinf_alpha_kappa(param *currentPar, param *tempPar, data *dat, dna_dis
 	  tempPar->kappa->values[toMove] = vec_int_i(currentPar->kappa,toMove);
 	  mcmcPar->n_reject_Tinf += 1;
 	  mcmcPar->n_reject_alpha += 1;
-	  mcmcPar->n_reject_kappa += 1;
 	}
       } /* end  ACCEPT/REJECT STEP */
     } /* end if ances > -1 */
