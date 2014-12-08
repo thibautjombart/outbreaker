d1e17b28534c3d9bb6146d2bb3a9ea7c7c9405bf
joel <j.hellewell14@imperial.ac.uk>
2014-12-08 14:15:56 +0000

    fixed: wasn't shaping group output alongside other output

--- tester.R
+++ tester.R
@@ -1,8 +1,8 @@
 library(igraph)
 library(outbreaker)	
 
-dm <- matrix(byrow=TRUE,ncol=3,c(0.998,0.001,0.001,0.001,0.998,0.001,0.001,0.001,0.998))
-res <- simOutbreak(R0=c(2,2.5,1),infec.curve=c(0,1,1,1),n.hosts=150,spatial=FALSE,group.sizes=c(50,50,50),trans.mat=dm,duration=8)
+dm <- matrix(byrow=TRUE,ncol=2,c(0,1,1,0))
+res <- simOutbreak(R0=c(2,2.5,1),infec.curve=c(0,1,1,1),n.hosts=40,spatial=FALSE,group.sizes=c(20,20),trans.mat=dm,duration=10)
 
 plot(as.igraph.simOutbreak(res,vertex.col="group"))
-legend(x="topleft",col=num2col(unique(res$group),col.pal=heat.colors),legend=c(1:3),pch=1)
+legend(x="topleft",col=num2col(unique(res$group),col.pal=funky),legend=c(1:4),pch=16)
