bf78cf2e5429ceec18eed5ee88e5ac80024f7fa0
joel <j.hellewell14@imperial.ac.uk>
2014-12-09 10:09:31 +0000

    graphs now plot with correct group colour

--- tester.R
+++ tester.R
@@ -1,8 +1,9 @@
 library(igraph)
 library(outbreaker)	
+library(adegenet)
 
 dm <- matrix(byrow=TRUE,ncol=2,c(0,1,1,0))
-res <- simOutbreak(R0=c(2,2.5,1),infec.curve=c(0,1,1,1),n.hosts=40,spatial=FALSE,group.sizes=c(20,20),trans.mat=dm,duration=10)
+res <- simOutbreak(R0=c(2,2.5,1),infec.curve=c(0,1,1,1),n.hosts=40,spatial=FALSE,group.sizes=c(20,20),trans.mat=dm,duration=5)
 
 plot(as.igraph.simOutbreak(res,vertex.col="group"))
-legend(x="topleft",col=num2col(unique(res$group),col.pal=funky),legend=c(1:4),pch=16)
+legend(x="topleft",col=num2col(unique(res$group),col.pal=funky),legend=c(1:2),pch=16)
