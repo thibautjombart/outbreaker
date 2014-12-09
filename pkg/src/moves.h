3d0ce20a87fa3333f750c23df4a27241da68acc1
joel <j.hellewell14@imperial.ac.uk>
2014-12-09 13:49:49 +0000

    begun move function for matrix

--- pkg/src/moves.h
+++ pkg/src/moves.h
@@ -43,6 +43,11 @@ void move_Tinf_alpha_kappa(param *currentPar, param *tempPar, data *dat, dna_dis
 
 void swap_ancestries(param *currentPar, param *tempPar, data *dat, dna_dist *dnaInfo, spatial_dist *spaInfo, gentime *gen, mcmc_param *mcmcPar, gsl_rng *rng);
 
+/* move for transmission matrix */
+void jiggle_trans_mat(param *currentPar, param *tempPar, data *dat, spatial_dist *spaInfo, mcmc_param *mcmcPar, gsl_rng *rng, int l);
+
+
+
 /* NO LONGER USED */
 /* void move_alpha(param *currentPar, param *tempPar, data *dat, dna_dist *dnaInfo, gentime *gen, mcmc_param *mcmcPar, gsl_rng *rng); */
 
