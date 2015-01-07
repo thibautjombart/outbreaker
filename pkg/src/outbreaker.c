3950542de1e6942dcf953e54384f37a307760615
joel <j.hellewell14@imperial.ac.uk>
2015-01-07 14:44:48 +0000

    I think the problem is that I set init.mu=0.5 when the data that was generated only has 1-3 changes in 10000, hence genetic likelihood was basically 0. Making init.mu more realistic fixed problem

--- pkg/src/outbreaker.c
+++ pkg/src/outbreaker.c
@@ -236,14 +236,16 @@ void test_R(unsigned char *DNAbinInput, int *Tcollec, int *n, int *nSeq, int *le
    Rprintf("\n loglikelihood_all: %f \n", loglikelihood_all(dat, dnaInfo, spatialInfo, gen, temp_par, rng));
    Rprintf("\n loglikelihood_local_i: %f \n", loglikelihood_local_i(2,dat, dnaInfo, spatialInfo, gen, temp_par, rng)); */
 
-    int caseno = 3;
+    int caseno = 4;
     int ances2 = find_sequenced_ancestor(caseno, dat, dnaInfo, par);
     Rprintf("\n ancestor: %d\n",ances2);
     Rprintf("\n kappa_temp: %d\n",par->kappa_temp);
 Rprintf("\n mu1:%f\n",par->mu1);
 Rprintf("\n mutation_ij:%d\n",mutation1_ij(caseno, ances2, dat, dnaInfo));
+Rprintf("\n matrix value: %d\n",mat_int_ij(dnaInfo->mutation1, 0, 0));
 Rprintf("\n com_nucl_ij:%d\n",com_nucl_ij(caseno, ances2, dat, dnaInfo));
-Rprintf("\n proba_mut: %f\n", proba_mut(0, 10000, 1, 0.5));
+Rprintf("\n proba_mut: %f\n", proba_mut(1, 9999, 1, 0.5));
+print_dna_dist(dnaInfo);
 
     double genlike = loglikelihood_gen_i(caseno,dat,dnaInfo,par,rng);
     Rprintf("\n Genetic likelihood for i: %f\n",genlike);
